098db467cb85e3305e0ca050849ab3d4702443cf
Vladimir Saakyan <Vgs0737372373@gmail.com>
2022-02-12 17:45:37 +0200

    first

---
+++ hw_1_compilation/task02_ant/app/src/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+import org.apache.commons.lang3.*;
+
+public class Test {
+    public void run(){
+        System.out.println("hello");
+        System.out.println(StringUtils.upperCase("Hello World"));
+    }
+}
